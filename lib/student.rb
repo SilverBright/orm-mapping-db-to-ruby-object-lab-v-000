2ca5237ecfb7f399ae4e483fa12ffa7fa797bd5b
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 00:49:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,13 +87,13 @@ class Student
       self.new_from_db(student)
     end
 
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(num)
        sql = <<-SQL
        SELECT *
        FROM students
        WHERE grade = ?
        SQL
-       DB[:conn].execute(sql).map do |row|
+       DB[:conn].execute(sql, num).map do |row|
          self.new_from_db(row)
       end
     end
