deafedf6cef745ffa9bc29a5bb750f224ac28459
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 00:38:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    student = self.first_X_students_in_grade_10(1).flatten
+    student = self.first_X_students_in_grade_10(1).to_s
       self.new_from_db(student)
   end
 
