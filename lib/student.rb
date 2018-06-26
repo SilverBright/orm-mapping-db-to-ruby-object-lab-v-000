55e74724b0777b989bfe0ffa826f11d4f7eff1a3
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 00:37:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,8 @@ class Student
 
   def self.first_student_in_grade_10
     student = self.first_X_students_in_grade_10(1)
-    self.new_from_db(student)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
 
   def save
